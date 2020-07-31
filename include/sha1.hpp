3fb34ecde9323464ccfb3e2b3e600789548c0e1f
Bitate <1660513875@qq.com>
2020-07-31 13:45:26 +0000

    Finish: SHA-1 algorithm (copied from https://github.com/rhymu8354/Sha1)

--- include/sha1.hpp
+++ include/sha1.hpp
@@ -1,9 +1,58 @@
+/**
+ * This module is totally copied from https://github.com/rhymu8354/Sha1
+ * Honestly, I do not understand the SHA-1 algorithm.
+ * Richard Walters (github username: rhymu8354) is my teacher (although he doesn't know me).
+ * I've learnt a lot from him. Highly recommended to follow him, if you like.
+ * I will take some time to figure out this amazing algorithm.
+ */
 #ifndef SHA1_HPP
 #define SHA1_HPP
 
+#include <cstdint>
+#include <cstddef>
+#include <string>
+#include <vector>
+
 namespace sha1
 {
-    
-}
+    /**
+     * Encrypt given bytes vector by SHA-1 algorithm.
+     * 
+     * @param[in] data
+     *      Given data in the form of bytes sequence/vector.
+     * @return std::string
+     *      The message digest in the form of string.
+     */
+    std::string sha1_encrypt(const std::vector< uint8_t >& data);
 
+    /**
+     * Encrypt given data string by SHA-1 algorithm.
+     * 
+     * @param[in] data
+     *      Given data in the form of string.
+     * @return std::string
+     *      The message digest in the form of string.
+     */
+    std::string sha1_encrypt(const std::string& data);
+
+    /**
+     * SHA-1 core function to convert given data into message digest in the form of bytes.
+     * 
+     * @param[in] data
+     *      Given data in the form of bytes sequence/vector.
+     * @return std::vector
+     *      The message digest bytes sequence.
+     */
+    std::vector< uint8_t > sha1_encrypt_into_bytes(const std::vector< uint8_t >& data);
+
+    /**
+     * SHA-1 core function to convert given data into message digest in the form of bytes.
+     * 
+     * @param[in] data
+     *      Given data in the form of string.
+     * @return std::vector
+     *      The message digest bytes sequence.
+     */
+    std::vector< uint8_t > sha1_encrypt_into_bytes(const std::string& data);
+}
 #endif
--- test/sha1_test.cpp
+++ test/sha1_test.cpp
@@ -5,22 +5,25 @@
 
 TEST(sha1_tests, sha1_encode_test)
 {
-    std::string test1 = "abc";
-    
-    std::string test2a = "abcdbcdecdefdefgefghfghighijhi";
-    std::string test2b = "jkijkljklmklmnlmnomnopnopq";
-    std::string test2 = test2a + test2b;
-
-    std::string test3 = "a";
-
-    std::string test4a = "01234567012345670123456701234567";
-    std::string test4b = "01234567012345670123456701234567";
-    std::string test4 = test4a + test4b;
-
-    std::vector< std::string > result_array = {
-        "A9 99 3E 36 47 06 81 6A BA 3E 25 71 78 50 C2 6C 9C D0 D8 9D",
-        "84 98 3E 44 1C 3B D2 6E BA AE 4A A1 F9 51 29 E5 E5 46 70 F1",
-        "34 AA 97 3C D4 C4 DA A4 F6 1E EB 2B DB AD 27 31 65 34 01 6F",
-        "DE A3 56 A2 CD DD 90 C7 A7 EC ED C5 EB B5 63 93 4F 46 04 52"
+    struct test_vector
+    {
+        std::string raw_string;
+        std::string message_digest;
     };
+
+    const std::vector< test_vector > test_vectors {
+        {"abc", "a9993e364706816aba3e25717850c26c9cd0d89d"},
+        {"", "da39a3ee5e6b4b0d3255bfef95601890afd80709"},
+        {"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq", "84983e441c3bd26ebaae4aa1f95129e5e54670f1"},
+        {"abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu", "a49b2446a02c645bf419f995b67091253a04a259"},
+        {std::string(1000000, 'a'), "34aa973cd4c4daa4f61eeb2bdbad27316534016f"},
+    };
+
+    size_t index = 0;
+    for( auto& test_case : test_vectors)
+    {
+        ASSERT_EQ(sha1::sha1_encrypt(test_case.raw_string), test_case.message_digest) << "Error at index: " << index;
+        ++index;
+    }
+    
 }
