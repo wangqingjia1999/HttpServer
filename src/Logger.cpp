4d01f7ebda09c352b12ae27945cd5d82f6a5ff61
Bitate <1660513875@qq.com>
2020-08-27 14:17:04 +0000

    Delete unused test cases

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,4 +1,5 @@
 #include "Logger.hpp"
+#include "Timer.hpp"
 
 #include <chrono>
 #include <string>
@@ -14,12 +15,6 @@ namespace Logger
 {
     void record(const std::string& message_string)
     {
-        time_t current_time = time(0);
-        // ctime() generate a date and time string but appending a '\n' at the last element
-        std::string current_time_string_tmp = ctime(&current_time);
-        // I just strip the last '\n' here
-        std::string current_time_string = current_time_string_tmp.substr(0, current_time_string_tmp.size() - 1);
-
         // Here path_buffer will contain the current working directory path (e.g. /home/bitate/HttpServer )
         char path_buffer[1024];
         if(getcwd(path_buffer, 1024) == nullptr)
@@ -31,7 +26,6 @@ namespace Logger
         std::string log_file_absolute_path = current_working_directory_string + "/log.txt";
         
         std::ofstream log_file_object(log_file_absolute_path, std::ios::app);
-        // log_file_object << '[' << current_time_string << "]  " << message_string << '\n';
         log_file_object << message_string << '\n';
     }
 }
